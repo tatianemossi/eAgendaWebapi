61223f72bfd37dbe6743f2d51a4e33ef98568660
Tatiane Mossi <mossitatiane2@gmail.com>
2022-09-26 19:56:07 -0300

    Adicionado configuração Identity no EntityFramework

--- eAgenda.Dominio/Compartilhado/EntidadeBase.cs
+++ eAgenda.Dominio/Compartilhado/EntidadeBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eAgenda.Dominio.ModuloAutenticacao;
+using System;
 using Taikandi;
 
 namespace eAgenda.Dominio.Compartilhado
@@ -6,10 +7,8 @@ namespace eAgenda.Dominio.Compartilhado
     public abstract class EntidadeBase<T>
     {
         public Guid Id { get; set; }
-
-        //public IdentityUser<Guid> Usuario { get; set; }
-
-        //public Guid UsuarioId { get; set; }
+        public Guid UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
 
         public EntidadeBase()
         {
