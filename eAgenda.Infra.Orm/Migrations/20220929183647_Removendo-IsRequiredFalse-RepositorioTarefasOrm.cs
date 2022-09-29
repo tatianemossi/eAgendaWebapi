591f45b426062f46f90aa3de92e3021c37e4da5f
Tatiane Mossi <mossitatiane2@gmail.com>
2022-09-29 15:37:41 -0300

    Removendo IsRequiredFalse do Repositorio de Tarefas Orm

---
+++ eAgenda.Infra.Orm/Migrations/20220929183647_Removendo-IsRequiredFalse-RepositorioTarefasOrm.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eAgenda.Infra.Orm.Migrations
+{
+    public partial class RemovendoIsRequiredFalseRepositorioTarefasOrm : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- eAgenda.Infra.Orm/ModuloTarefa/MapeadorTarefaOrm.cs
+++ eAgenda.Infra.Orm/ModuloTarefa/MapeadorTarefaOrm.cs
@@ -18,7 +18,7 @@ namespace eAgenda.Infra.Orm.ModuloTarefa
 
             builder.HasOne(x => x.Usuario)
                 .WithMany()
-                .IsRequired(false)
+                .IsRequired()
                 .HasForeignKey(x => x.UsuarioId)
                 .OnDelete(DeleteBehavior.NoAction);
 
