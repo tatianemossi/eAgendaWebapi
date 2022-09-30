43c6430d41e4916429fbb11612686d06773a4e12
Tatiane Mossi <mossitatiane2@gmail.com>
2022-09-30 15:39:03 -0300

    Refatoração de TarefaProfile com o IMappingAction.

--- eAgenda.Webapi/Config/AutoMapperConfig/TarefaProfile.cs
+++ eAgenda.Webapi/Config/AutoMapperConfig/TarefaProfile.cs
@@ -1,11 +1,7 @@
 ﻿using AutoMapper;
 using eAgenda.Dominio.Compartilhado;
-using eAgenda.Dominio.ModuloAutenticacao;
 using eAgenda.Dominio.ModuloTarefa;
 using eAgenda.Webapi.ViewModels.ModuloTarefa;
-using Microsoft.AspNetCore.Http;
-using System;
-using System.Security.Claims;
 
 namespace eAgenda.Webapi.Config.AutoMapperConfig
 {
@@ -22,44 +18,11 @@ namespace eAgenda.Webapi.Config.AutoMapperConfig
             CreateMap<InserirTarefaViewModel, Tarefa>()
                 .ForMember(destino => destino.UsuarioId, opt => opt.MapFrom<UsuarioResolver>())
                 .ForMember(destino => destino.Itens, opt => opt.Ignore())
-                .AfterMap((viewModel, tarefa) =>
-                {
-                    if (viewModel.Itens == null)
-                        return;
-
-                    foreach (var itemVM in viewModel.Itens)
-                    {
-                        var item = new ItemTarefa();
-
-                        item.Titulo = itemVM.Titulo;
-
-                        tarefa.AdicionarItem(item);
-                    }
-                });
+                .AfterMap<AdicionarItensMappingAction>();
 
             CreateMap<EditarTarefaViewModel, Tarefa>()
                 .ForMember(destino => destino.Itens, opt => opt.Ignore())
-                .AfterMap((viewModel, tarefa) =>
-            {
-                foreach (var itemVM in viewModel.Itens)
-                {
-                    if (itemVM.Concluido)
-                        tarefa.ConcluirItem(itemVM.Id);
-                    else
-                        tarefa.MarcarPendente(itemVM.Id);
-                }
-
-                foreach (var itemVM in viewModel.Itens)
-                {
-                    if (itemVM.Status == StatusItemTarefa.Adicionado)
-                    {
-                        var item = new ItemTarefa(itemVM.Titulo);
-                        tarefa.AdicionarItem(item);
-                    }
-                    else if (itemVM.Status == StatusItemTarefa.Removido)
-                        tarefa.RemoverItem(itemVM.Id);
-                }
-            });
+                .AfterMap<EditarItensMappingAction>();
         }
 
         private void ConverterDeEntidadeParaViewModel()
@@ -78,6 +41,49 @@ namespace eAgenda.Webapi.Config.AutoMapperConfig
             CreateMap<ItemTarefa, VisualizarItemTarefaViewModel>()
                 .ForMember(destino => destino.Situacao, opt =>
                     opt.MapFrom(origem => origem.Concluido ? "Concluído" : "Pendente"));
+        }
+    }
+
+    public class AdicionarItensMappingAction : IMappingAction<InserirTarefaViewModel, Tarefa>
+    {
+        public void Process(InserirTarefaViewModel viewModel, Tarefa tarefa, ResolutionContext context)
+        {
+            if (viewModel.Itens == null)
+                return;
+
+            foreach (var itemVM in viewModel.Itens)
+            {
+                var item = new ItemTarefa();
+
+                item.Titulo = itemVM.Titulo;
+
+                tarefa.AdicionarItem(item);
+            }
+        }
+    }
+
+    public class EditarItensMappingAction : IMappingAction<EditarTarefaViewModel, Tarefa>
+    {
+        public void Process(EditarTarefaViewModel viewModel, Tarefa tarefa, ResolutionContext context)
+        {
+            foreach (var itemVM in viewModel.Itens)
+            {
+                if (itemVM.Concluido)
+                    tarefa.ConcluirItem(itemVM.Id);
+                else
+                    tarefa.MarcarPendente(itemVM.Id);
+            }
+
+            foreach (var itemVM in viewModel.Itens)
+            {
+                if (itemVM.Status == StatusItemTarefa.Adicionado)
+                {
+                    var item = new ItemTarefa(itemVM.Titulo);
+                    tarefa.AdicionarItem(item);
+                }
+                else if (itemVM.Status == StatusItemTarefa.Removido)
+                    tarefa.RemoverItem(itemVM.Id);
+            }
         }
     }
 }
